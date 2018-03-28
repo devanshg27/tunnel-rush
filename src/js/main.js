597c182d466a500d38c3f7ea055e17b4a77d4ecf
Devansh Gautam <devanshg27@gmail.com>
2018-03-28 13:08:10 +0530

    Restructured cube initialization and drawing code

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ import fragShaderSource from 'shaders/fragment_shader.glsl';
 
 var shaders = require('./shader.js');
 var glm = require('gl-matrix');
-
-console.log('Started');
+var cubeHelper = require('./cube.js');
+var cube1, cube2;
 
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
@@ -35,8 +35,9 @@ else {
 
 	// Here's where we call the routine that builds all the
 	// objects we'll be drawing.
-	const buffers = initBuffers(gl);
-
+	cubeHelper.initBuffers(gl);
+	cube1 = new cubeHelper.makeCube([0, 0, -6], 0);
+	cube2 = new cubeHelper.makeCube([1, 1, -6], 0);
 
 	// Draw the scene repeatedly
 	var then = 0;
@@ -45,132 +46,11 @@ else {
 		const deltaTime = now - then;
 		then = now;
 
-		drawScene(gl, programInfo, buffers, deltaTime);
+		drawScene(gl, programInfo, deltaTime);
 
 		requestAnimationFrame(render);
 	}
 	requestAnimationFrame(render);
-}
-
-//
-// initBuffers
-//
-// Initialize the buffers we'll need. For this demo, we just
-// have one object -- a simple three-dimensional cube.
-//
-function initBuffers(gl) {
-
-	// Create a buffer for the cube's vertex positions.
-
-	const positionBuffer = gl.createBuffer();
-
-	// Select the positionBuffer as the one to apply buffer
-	// operations to from here out.
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-	// Now create an array of positions for the cube.
-
-	const positions = [
-		// Front face
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0, -1.0, -1.0,
-
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		 1.0,  1.0, -1.0,
-
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		 1.0, -1.0, -1.0,
-		 1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-
-		// Right face
-		 1.0, -1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0,  1.0,  1.0,
-		 1.0, -1.0,  1.0,
-
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0,
-	];
-
-	// Now pass the list of positions into WebGL to build the
-	// shape. We do this by creating a Float32Array from the
-	// JavaScript array, then use it to fill the current buffer.
-
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-	// Now set up the colors for the faces. We'll use solid colors
-	// for each face.
-
-	const faceColors = [
-		[1.0,  1.0,  1.0,  1.0],    // Front face: white
-		[1.0,  0.0,  0.0,  1.0],    // Back face: red
-		[0.0,  1.0,  0.0,  1.0],    // Top face: green
-		[0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-		[1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-		[1.0,  0.0,  1.0,  1.0],    // Left face: purple
-	];
-
-	// Convert the array of colors into a table for all the vertices.
-
-	var colors = [];
-
-	for (var j = 0; j < faceColors.length; ++j) {
-		const c = faceColors[j];
-
-		// Repeat each color four times for the four vertices of the face
-		colors = colors.concat(c, c, c, c);
-	}
-
-	const colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-	// Build the element array buffer; this specifies the indices
-	// into the vertex arrays for each face's vertices.
-
-	const indexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-	// This array defines each face as two triangles, using the
-	// indices into the vertex array to specify each triangle's
-	// position.
-
-	const indices = [
-		0,  1,  2,      0,  2,  3,    // front
-		4,  5,  6,      4,  6,  7,    // back
-		8,  9,  10,     8,  10, 11,   // top
-		12, 13, 14,     12, 14, 15,   // bottom
-		16, 17, 18,     16, 18, 19,   // right
-		20, 21, 22,     20, 22, 23,   // left
-	];
-
-	// Now send the element array to GL
-
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-			new Uint16Array(indices), gl.STATIC_DRAW);
-
-	return {
-		position: positionBuffer,
-		color: colorBuffer,
-		indices: indexBuffer,
-	};
 }
 
 var cubeRotation = 0;
@@ -178,7 +58,7 @@ var cubeRotation = 0;
 //
 // Draw the scene.
 //
-function drawScene(gl, programInfo, buffers, deltaTime) {
+function drawScene(gl, programInfo, deltaTime) {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
 	gl.clearDepth(1.0);                 // Clear everything
 	gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -198,8 +78,9 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 	const fieldOfView = 45 * Math.PI / 180;   // in radians
 	const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
 	const zNear = 0.1;
-	const zFar = 100.0;
+	const zFar = 1000.0;
 	const projectionMatrix = glm.mat4.create();
+	const tempMatrix = glm.mat4.create();
 
 	// note: glmatrix.js always has the first argument
 	// as the destination to receive the result.
@@ -209,69 +90,8 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 									 zNear,
 									 zFar);
 
-	// Set the drawing position to the "identity" point, which is
-	// the center of the scene.
-	const modelViewMatrix = glm.mat4.create();
-
-	// Now move the drawing position a bit to where we want to
-	// start drawing the square.
-
-	glm.mat4.translate(modelViewMatrix,     // destination matrix
-								 modelViewMatrix,     // matrix to translate
-								 [-0.0, 0.0, -6.0]);  // amount to translate
-	glm.mat4.rotate(modelViewMatrix,  // destination matrix
-							modelViewMatrix,  // matrix to rotate
-							cubeRotation,     // amount to rotate in radians
-							[0, 0, 1]);       // axis to rotate around (Z)
-	glm.mat4.rotate(modelViewMatrix,  // destination matrix
-							modelViewMatrix,  // matrix to rotate
-							cubeRotation * .7,// amount to rotate in radians
-							[0, 1, 0]);       // axis to rotate around (X)
-
-	// Tell WebGL how to pull out the positions from the position
-	// buffer into the vertexPosition attribute
-	{
-		const numComponents = 3;
-		const type = gl.FLOAT;
-		const normalize = false;
-		const stride = 0;
-		const offset = 0;
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-		gl.vertexAttribPointer(
-				programInfo.attribLocations.vertexPosition,
-				numComponents,
-				type,
-				normalize,
-				stride,
-				offset);
-		gl.enableVertexAttribArray(
-				programInfo.attribLocations.vertexPosition);
-	}
-
-	// Tell WebGL how to pull out the colors from the color buffer
-	// into the vertexColor attribute.
-	{
-		const numComponents = 4;
-		const type = gl.FLOAT;
-		const normalize = false;
-		const stride = 0;
-		const offset = 0;
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-		gl.vertexAttribPointer(
-				programInfo.attribLocations.vertexColor,
-				numComponents,
-				type,
-				normalize,
-				stride,
-				offset);
-		gl.enableVertexAttribArray(
-				programInfo.attribLocations.vertexColor);
-	}
-
-	// Tell WebGL which indices to use to index the vertices
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-
-	// Tell WebGL to use our program when drawing
+	glm.mat4.lookAt(tempMatrix, [0, 0, cubeRotation], [0,0,1], [0,1,0]);
+	glm.mat4.multiply(projectionMatrix, projectionMatrix, tempMatrix);
 
 	gl.useProgram(programInfo.program);
 
@@ -281,17 +101,9 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 			programInfo.uniformLocations.projectionMatrix,
 			false,
 			projectionMatrix);
-	gl.uniformMatrix4fv(
-			programInfo.uniformLocations.modelViewMatrix,
-			false,
-			modelViewMatrix);
 
-	{
-		const vertexCount = 36;
-		const type = gl.UNSIGNED_SHORT;
-		const offset = 0;
-		gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-	}
+	cubeHelper.draw(gl, programInfo, cube1);
+	cubeHelper.draw(gl, programInfo, cube2);
 
 	// Update the rotation for the next draw
 
